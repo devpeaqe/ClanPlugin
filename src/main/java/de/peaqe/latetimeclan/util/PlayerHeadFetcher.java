74e60e72a4e827c089aa4ef83d458c3898dff7f8
peaqe <contact@peaqe.dev>
2024-02-23 21:07:42 +0100

    GUI Creation ===================
    * Addet description to the methods

--- src/main/java/de/peaqe/latetimeclan/util/PlayerHeadFetcher.java
+++ src/main/java/de/peaqe/latetimeclan/util/PlayerHeadFetcher.java
@@ -2,6 +2,7 @@ package de.peaqe.latetimeclan.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.util.io.BukkitObjectInputStream;
@@ -14,24 +15,28 @@ import java.io.IOException;
 import java.util.UUID;
 
 /**
- * *
+ *
+ * PlayerHeadFetcher fetches player heads as ItemStacks and provides methods to serialize and deserialize them.
  *
  * @author peaqe
  * @version 1.0
  * @since 21.02.2024 | 19:40 Uhr
- * *
+ *
  */
-
 public class PlayerHeadFetcher {
 
+    /**
+     * Retrieves the head of the player with the given UUID.
+     *
+     * @param playerUUID The UUID of the player.
+     * @return The player head as an ItemStack.
+     */
     public static ItemStack getPlayerHeadFromUUID(UUID playerUUID) {
-
-        var playerHead = new ItemStack(Material.PLAYER_HEAD);
-        var meta = (SkullMeta) playerHead.getItemMeta();
+        ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) playerHead.getItemMeta();
 
         if (meta != null) {
-
-            var targetPlayer = Bukkit.getOfflinePlayer(playerUUID);
+            OfflinePlayer targetPlayer = Bukkit.getOfflinePlayer(playerUUID);
             if (!targetPlayer.hasPlayedBefore()) {
                 targetPlayer = Bukkit.getOfflinePlayer("peaqe");
             }
@@ -43,6 +48,13 @@ public class PlayerHeadFetcher {
         return playerHead;
     }
 
+    /**
+     * Converts an ItemStack to a Base64-encoded string.
+     *
+     * @param item The ItemStack to serialize.
+     * @return The Base64-encoded string representation of the ItemStack.
+     * @throws IllegalStateException If unable to save the ItemStack.
+     */
     public static String toBase64(ItemStack item) throws IllegalStateException {
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -58,6 +70,13 @@ public class PlayerHeadFetcher {
         }
     }
 
+    /**
+     * Converts a Base64-encoded string back to an ItemStack.
+     *
+     * @param data The Base64-encoded string.
+     * @return The deserialized ItemStack.
+     * @throws IOException If unable to decode the string or read the ItemStack.
+     */
     public static ItemStack fromBase64(String data) throws IOException {
         try {
             ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
@@ -72,5 +91,4 @@ public class PlayerHeadFetcher {
             throw new IOException("Unable to decode class type.", e);
         }
     }
-
 }
